7901c5151ea46147b2745935f552158ab15fc66c
ponta-yu <na.yu.sweets.1408@gmail.com>
2020-03-10 21:29:19 +0900

    サウンド再生機能を追加

--- SampleQuiz/SampleQuiz/ViewController.swift
+++ SampleQuiz/SampleQuiz/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    /* システムサウンド */
+    @IBAction func tapSystemSound(_ sender: Any) {
+        /* システムサウンドを鳴らし、バイブレーションさせる */
+        AudioServicesPlaySystemSoundWithCompletion(1005) {
+            
+        }
+    }
+    
+    /* カスタムサウンド */
+    @IBAction func tapCustomSound(_ sender: Any) {
+        /* サウンドファイルの設定 */
+        let soundUrl = Bundle.main.url(forResource: "thunder03", withExtension: "mp3")
+        /* サウンドIDを登録するための変数を定義 */
+        var soundId: SystemSoundID = 0
+        /* サウンドファイルを登録してサウンドIDを取得 */
+        AudioServicesCreateSystemSoundID(soundUrl! as CFURL, &soundId)
+        /* 取得したサウンドIDを使用してサウンドを鳴らす */
+        AudioServicesPlaySystemSoundWithCompletion(soundId){
+            
+        }
+    }
+    
+    /* バイブレーション */
+    @IBAction func tapVibration(_ sender: Any) {
+        AudioServicesPlaySystemSoundWithCompletion(kSystemSoundID_Vibrate) {
+            
+        }
+    }
 }
 
